2bf7580d174f49620e8e57d0ad0f02cda687b8a7
HaiderAbas <haiderabbas.gb5@gmail.com>
2023-02-25 01:05:44 +0500

    completed navbar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
+import Footer from '@/components/footer/Footer'
+import Navbar from '@/components/navbar/Navbar'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className='container mx-auto font-sans'>
+      <Navbar />
+      <main>
+        <Component {...pageProps} />
+      </main>
+      <Footer />
+    </div>
+  )
 }
